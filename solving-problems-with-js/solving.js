3411986a87b73c4395799879a972923f38a88f1c
mullerow <maximilian.kohler@posteo.de>
2024-02-20 10:22:18 +0100

    feat: complex wolf-object added

--- solving-problems-with-js/solving.js
+++ solving-problems-with-js/solving.js
@@ -1,1 +1,42 @@
 console.log("her mit den Problemen!");
+
+const testObject = {
+  ordnung: "Raubtiere",
+  unterordnung: "Hundeartige",
+  familie: "Hunde",
+  tribus: "echte Hunde",
+  gattung: "wolfs- und Schkalartige",
+  art: "wolf",
+  tier1: {
+    name: "Steffen",
+    alter: 8,
+    geschlecht: "männlich",
+    wohnhaft: "duisburg",
+    kennungsId: 12341,
+    lebend: true,
+  },
+  tier2: {
+    name: "Olga",
+    alter: 2,
+    geschlecht: "weiblich",
+    wohnhaft: "Eisenhüttenstadt",
+    kennungsId: 12342,
+    lebend: true,
+  },
+  tier3: {
+    name: "Richard",
+    alter: 14,
+    geschlecht: "männlich",
+    wohnhaft: "Recklinghausen",
+    kennungsId: 12343,
+    lebend: false,
+  },
+};
+
+console.log(testObject.tier1.alter);
+
+console.log(testObject.tier2);
+
+testObject.newProperty = "new Entry";
+
+console.log(testObject.newProperty);
